e4e0daae27d53328fd13dc974f0c51e37211a86b
Tyler Bertrand <132798327+tyler8265@users.noreply.github.com>
2026-04-27 20:50:28 -0400

    function for pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -3,12 +3,13 @@ import cleaner
 import db_manager
 import sqlite3
 
-if __name__ == '__main__':
-  conn = sqlite3.connect('database/nba_predictor.db')
+def run():
+  if __name__ == '__main__':
+    conn = sqlite3.connect('database/nba_predictor.db')
 
-  stats = data_fetcher.fetch_stats()
-  injuries = data_fetcher.fetch_injuries()
-  cleaner.cleaner()
-  db_manager.bulk_insert(conn, 'player_stats', stats.to_dict('records'))
-  db_manager.bulk_insert(conn, 'players_injuries', injuries.to_dict('injuries'))
+    stats = data_fetcher.fetch_stats()
+    injuries = data_fetcher.fetch_injuries()
+    cleaner.cleaner()
+    db_manager.bulk_insert(conn, 'player_stats', stats.to_dict('records'))
+    db_manager.bulk_insert(conn, 'players_injuries', injuries.to_dict('injuries'))
 
